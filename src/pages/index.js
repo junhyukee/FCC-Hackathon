8145a4bd325769b60b58b48ad0a99dc68f503eac
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-08-29 14:13:18 -0700

    change link to a tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ const IndexPage = () => (
     <h1>Hello everyone!</h1>
     <p>My name is Jun, and I'm a software engineer.</p>
     <p>Nice to meet you!</p>
-    <Link to="https://junhyukkim.com">Site</Link>
+    <a href="https://junhyukkim.com">Site</a>
   </div>
 )
 
