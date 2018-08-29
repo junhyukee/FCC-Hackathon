4713a0774d0c90494745d5405c7dd3e944abb165
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-08-29 14:00:54 -0700

    commit for FCC hackathon entry

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hello everyone!</h1>
+    <p>My name is Jun, and I'm a software engineer.</p>
+    <p>Nice to meet you!</p>
+    <Link to="https://junhyukkim.com">Site</Link>
+  </div>
+)
+
+export default IndexPage
